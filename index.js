b7d5730d569d9e9945168e4873df3d2239cc7791
riceknot <118870849+riceknot@users.noreply.github.com>
2023-05-19 23:00:35 +0700

    Add files via upload

--- index.js
+++ index.js
@@ -44,6 +44,10 @@ app.get('/product-detail', (req, res) => {
     res.render('product-detail');
 });
 
+//---------------------------Shipping list:----------------------------
+app.get('/Shipper/shipping-list', (req, res) => {
+    res.render('shipper');
+});
 
 //-----------------Render My Account page with data based on the user's:-----------------
 //For customer:
@@ -89,7 +93,10 @@ app.post('/logout', (req, res) => {
 //------------------Schemas:----------------------
 //User information:
 const userSchema = new mongoose.Schema({
-    role: String,
+    role: {
+        type: String,
+        required: true,
+    },
     username: {
         type: String,
         minlength: 8,
@@ -100,6 +107,9 @@ const userSchema = new mongoose.Schema({
     },
     password: {
         type: String,
+        minlength: 8,
+        maxlength: 20,
+        match: /^[a-zA-Z0-9!@#$%^&*]+$/, //Password only allows letters, digits and !@#$%^&*.
         required: true
     },
     full_name: {
@@ -126,6 +136,7 @@ const userSchema = new mongoose.Schema({
         mimeType: String,
     }
 });
+//----------------------
 
 //Product information:
 const productSchema = new mongoose.Schema({
@@ -150,14 +161,52 @@ const cartSchema = new mongoose.Schema({
         mimeType: String,
     },
     description: String
-})
+});
 
-//
+//Shipping list:
+const shippingSchema = new mongoose.Schema({
+    product_id: String,
+    product_name: String,
+    product_price: String,
+    product_img: {
+        data: Buffer,
+        mimeType: String
+    },
+    customer_name: String,
+    customer_address: String
+});
+
+const orderedItemSchema = new mongoose.Schema({
+    product_id: String,
+    name: String,
+    price: String,
+    quantity: Number,
+    image: {
+        data: Buffer,
+        mimeType: String,
+    },
+    description: String
+});
+
+const orderSchema = new mongoose.Schema({
+    customer_id: String,
+    shipper_id: String,
+    distribution_hub: String,
+    status: String,
+    total_price: Number,
+    items: [orderedItemSchema],
+    address: String
+});
+
+
+
 //------------------------------------------------
 
 const User = mongoose.model('User', userSchema);
 const Product = mongoose.model('Product', productSchema);
-
+//experimental:
+const Order = mongoose.model('Order', orderSchema);
+const OrderItem = mongoose.model('OrderItem', orderedItemSchema);
 
 //--------------Registration form:--------------------
 //User register:
@@ -169,7 +218,7 @@ app.post('/register', async (req, res) => {
         const user = new User({
             role: req.body.role,
             username: req.body.username,
-            password: hasedPASS,
+            password: hasedPASS,         //Storing the hased password. 
             full_name: req.body.full_name,
             address: req.body.address,
             distribution_hub: req.body.distribution_hub,
@@ -181,9 +230,9 @@ app.post('/register', async (req, res) => {
             }
         });
 
-        await user.save()
+        await user.save()  //Save the data into the User collection.
             .then(() => {
-                res.redirect('/login');
+                res.redirect('/login');  //Redirect the user to the login page.
             })
             .catch((error) => {
                 console.log(error.message);
@@ -218,6 +267,37 @@ app.post('/login', async (req, res) => {
 
 //-----------------------------------------------------
 
+//----------------My Account Profile Picture Update:------------
+app.post('/:user_role/:user_id/pfp-update', (req, res) => {
+    User.findByIdAndUpdate(req.params.user_id, {
+        profilePicture: {
+            data: req.files.img_change.data,
+            mimeType: req.files.img_change.mimetype
+        }
+    }, { new: true })
+        .then((updatedUser) => {
+            res.redirect(`/${updatedUser.role}/${updatedUser._id}`);
+        })
+        .catch((error) => {
+            console.log(error.message);
+        })
+});
+//---------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 //--------------------EXPERIMENTAL!!!!!---------------------------
 //Adding products:
 app.post(`/Vendor/:id/products/update`, (req, res) => {
@@ -241,7 +321,7 @@ app.post(`/Vendor/:id/products/update`, (req, res) => {
         });
 });
 
-//View products:
+//-----------------View products:----------------------------
 app.get(`/Vendor/:id/products`, (req, res) => {
     const user = User.findById(req.params.id)
         .then((user) => {
@@ -283,6 +363,129 @@ app.get('/Customer/:customer_id/product-detail/:product_id', (req, res) => {
             console.log(error.message);
         });
 });
+//---------------------------------------------------
+
+
+
+//----------EXPERIMENTAL ORDER------------------
+// Display all products
+app.get('/Customer/:id/products', (req, res) => {
+    Product.find()
+        .then((products) => {
+            res.render('customer-products', { products });
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+});
+
+// Filter products based on name and price 
+app.post('/Customer/:id/products/search', (req, res) => {
+    const { minPrice, maxPrice, productName } = req.body;
+    const query = {
+        price: { $gte: minPrice, $lte: maxPrice },
+        name: { $regex: productName, $options: 'i' },
+    };
+
+    Product.find(query)
+        .then((products) => {
+            res.render('customer-products', { products });
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+});
+
+// Add a product to the shopping cart
+app.post('/Customer/:customer_id/add-to-cart/:product_id', (req, res) => {
+    Product.findById(req.params.product_id)
+        .then((product) => {
+            const cartItem = new Cart({
+                customer_id: req.params.customer_id,
+                vendor_id: product.vendor_id,
+                name: product.name,
+                price: product.price,
+                image: product.image,
+                description: product.description
+            });
+
+            cartItem.save()
+                .then(() => {
+                    res.redirect(`/Customer/${req.params.customer_id}/products`);
+                })
+                .catch((error) => {
+                    console.log(error.message);
+                });
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+});
+
+// Place an order
+app.post('/Customer/:id/place-order', (req, res) => {
+    Cart.find({ customer_id: req.params.id })
+        .then(async (cartItems) => {
+            const orderItems = cartItems.map((item) => {
+                return new OrderItem({
+                    product_id: item.product_id,
+                    name: item.name,
+                    price: item.price,
+                    quantity: 1,
+                    image: item.image,
+                    description: item.description
+                });
+            });
+
+            const totalPrice = orderItems.reduce((sum, item) => sum + parseFloat(item.price), 0);
+
+            const customer = await User.findById(req.params.id);
+
+            const order = new Order({
+                customer_id: req.params.id,
+                shipper_id: null,
+                distribution_hub: customer.distribution_hub,
+                status: 'active',
+                total_price: totalPrice,
+                items: orderItems,
+                address: customer.address
+            });
+
+            await order.save();
+
+            await Cart.deleteMany({ customer_id: req.params.id });
+
+            res.redirect(`/Customer/${req.params.id}/orders`);
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+});
+
+// Display active orders for shippers
+app.get('/Shipper/:id/orders', (req, res) => {
+    Order.find({ shipper_id: req.params.id, status: 'active' })
+        .then((orders) => {
+            res.render('shipper-orders', { orders });
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+});
+
+// Update the order status
+app.post('/Shipper/:shipper_id/update-order/:order_id', (req, res) => {
+    const newStatus = req.body.status;
+
+    Order.findByIdAndUpdate(req.params.order_id, { status: newStatus })
+        .then(() => {
+            res.redirect(`/Shipper/${req.params.shipper_id}/orders`);
+        })
+        .catch((error) => {
+            console.log(error.message);
+        });
+});
+//--------------------------------------------------  
 
 
 
